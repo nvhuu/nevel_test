3ee0eb9df718e4aa0c433bdffc9ca235ff9df467
huunguyen <nvhuu1998@gmail.com>
2024-11-18 15:28:57 +0700

    hide header when scroll down

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,10 +1,36 @@
+"use client";
 import Logo from "@/public/images/logo.png";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import NavBar from "../NavBar";
 import "./styles.scss";
 export default function Header() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
   return (
-    <div className="header w-full">
+    <div
+      className="header w-full"
+      style={{ transform: isVisible ? "translateY(0)" : "translateY(-100%)" }}>
       <div className="header__content h-full w-full">
         <div className="container h-full flex items-baseline justify-between md:items-center z-10">
           <Image
